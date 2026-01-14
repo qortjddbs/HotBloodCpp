06f985c065aa2e3936a2a29f448b5cef1b4f9fce
qortjddbs(notebook) <beak980513@naver.com>
2026-01-14 19:02:01 +0900

    템플릿 이용 (데이터 손실 x)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 // 01-4 인라인(inline) 함수
 #include <iostream>
 
-inline int SQUARE(int x) {
+template <typename T>
+inline T SQUARE(T x) {
 	return x * x;
 }
 
 int main(void) {
-	std::cout << SQUARE(5) << std::endl;
+	std::cout << SQUARE(5.5) << std::endl;
 	std::cout << SQUARE(12) << std::endl;
 	return 0;
 }
