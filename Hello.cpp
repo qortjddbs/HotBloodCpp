47374ed11badc82d46f7fa059c59118ef03e7072
qortjddbs(notebook) <beak980513@naver.com>
2026-01-14 16:16:19 +0900

    예제 1-5

--- Hello.cpp
+++ Hello.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 
 int main(void) {
-	int val1, val2;
-	int result = 0;
-	std::cout << "두 개의 숫자입력 : ";
-	std::cin >> val1 >> val2;
+	char name[100];
+	char lang[200];
 
-	if (val1 < val2) {
-		for (int i = val1 + 1; i < val2; ++i) {
-			result += i;
-		}
-	}
-	else {
-		for (int i = val2 + 1; i < val1; ++i) {
-			result += i;
-		}
-	}
+	std::cout << "이름은 무엇입니까? ";
+	std::cin >> name;
 
-	std::cout << "두 수 사이의 정수 합 : " << result << std::endl;
+	std::cout << "좋아하는 프로그래밍 언어는 무엇인가요? ";
+	std::cin >> lang;
+
+	std::cout << "내 이름은 " << name << "입니다. \n";
+	std::cout << "제일 좋아하는 언어는 " << lang << "입니다." << std::endl;
 
 	return 0;
 }
