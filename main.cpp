120190ae41c2b52a655b646e24bf1dc8e09e7891
qortjddbs(notebook) <beak980513@naver.com>
2026-01-14 19:39:58 +0900

    예제

--- main.cpp
+++ main.cpp
@@ -5,19 +5,28 @@ namespace BestComImpl {
 	void SimpleFunc(void);
 }
 
+namespace BestComImpl {
+	void PrettyFunc(void);
+}
+
 namespace ProgComImpl {
 	void SimpleFunc(void);
 }
 
 int main(void) {
 	BestComImpl::SimpleFunc();
-	ProgComImpl::SimpleFunc();
 
 	return 0;
 }
 
 void BestComImpl::SimpleFunc(void) {
 	std::cout << "BestCom이 정의한 함수" << std::endl;
+	PrettyFunc();
+	ProgComImpl::SimpleFunc();
+}
+
+void BestComImpl::PrettyFunc(void) {
+	std::cout << "So Pretty!!" << std::endl;
 }
 
 void ProgComImpl::SimpleFunc(void) {
