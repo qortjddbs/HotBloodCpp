5ec43a89a3c2ac32c34e262d0604755123a2dcae
qortjddbs(notebook) <beak980513@naver.com>
2026-01-14 16:37:00 +0900

    문제 01-1 (문제4) 이게 더 간단할듯

--- problem.cpp
+++ problem.cpp
@@ -7,13 +7,11 @@ int main(void) {
 	while (1) {
 		std::cout << "판매 금액을 만원 단위로 입력(-1 to end) : ";
 		std::cin >> sale;
-		if (sale == -1) {
-			std::cout << "프로그램을 종료합니다." << std::endl;
-			return 0;
-		}
+		if (sale == -1) break;
 		salary = 50 + sale * 0.12;
 		std::cout << "이번 달 급여 : " << salary << "만원" << std::endl;
 	}
+	std::cout << "프로그램을 종료합니다. \n";
 
 	return 0;
 }
