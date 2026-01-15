00c0fa6f9f81bee5bde96b1c47b3705ae72c7276
qortjddbs(notebook) <beak980513@naver.com>
2026-01-15 18:45:15 +0900

    내껀(주석처리) 틀린 답. 나는 ref가 num을 직접 참조하고 있음.

--- p02-2.cpp
+++ p02-2.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
 
+//int main(void) {
+//	const int num = 12;
+//	const int* ptr = &num;
+//	const int& ref = *ptr;
+//
+//	cout << num << endl;
+//	cout << *ptr << endl;
+//	cout << ref << endl;
+//
+//	return 0;
+//}
+
 int main(void) {
 	const int num = 12;
 	const int* ptr = &num;
-	const int& ref = *ptr;
+	const int *(&ref) = ptr;
 
-	cout << num << endl;
 	cout << *ptr << endl;
-	cout << ref << endl;
+	cout << *ref << endl;
 
 	return 0;
 }
