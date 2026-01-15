d93a6250edebb79e14c71903f0ddc13006c54567
qortjddbs(notebook) <beak980513@naver.com>
2026-01-15 19:39:58 +0900

    문제 1

---
+++ p02-4.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(void) {
+	char str1[100] = "I am strong!";
+	char str2[30];
+
+	strcpy(str2, str1);
+	int len = strlen(str1);
+	cout << "str1 : " << str1 << endl;
+	cout << "str2 : " << str2 << endl;
+	cout << "str1의 길이 : "<< len << endl;
+	if (strcmp(str1, str2) == 0) cout << "두 문장은 일치합니다!" << endl;
+	else if (strcmp(str1, str2) < 0) cout << "str1이 더 기네요! 하하" << endl;
+	else cout << "str2가 더 길었습니다~" << endl;
+	strcat(str1, str2);
+	cout << "str1 : " << str1 << endl;
+	if (strcmp(str1, str2) == 0) cout << "두 문장은 일치합니다!" << endl;
+	else if (strcmp(str1, str2) > 0) cout << "str1이 더 기네요! 하하" << endl;
+	else cout << "str2가 더 길었습니다~" << endl;
+
+	return 0;
+}
