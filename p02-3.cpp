5ed34c47ff3f409be0bbf1a681d6e5e94e7495f9
qortjddbs(notebook) <beak980513@naver.com>
2026-01-15 19:17:27 +0900

    문제02-3

---
+++ p02-3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+typedef struct __Point {
+	int xpos;
+	int ypos;
+} Point;
+
+Point& PntAdder(const Point& p1, const Point& p2);
+
+int main(void) {
+	Point *p1 = new Point;
+	p1->xpos = 5; p1->ypos = 5;
+	Point* p2 = new Point;
+	p2->xpos = 3; p2->ypos = -2;
+	Point* add = new Point;
+	*add = PntAdder(*p1, *p2);
+	cout << p1->xpos << ", " << p1->ypos << endl;
+	cout << p2->xpos << ", " << p2->ypos << endl;
+	cout << add->xpos << ", " << add->ypos << endl;
+
+	return 0;
+}
+
+Point& PntAdder(const Point& p1, const Point& p2) {
+	Point* temp = new Point;
+	temp->xpos = p1.xpos + p2.xpos;
+	temp->ypos = p1.ypos + p2.ypos;
+	return *temp;
+}
