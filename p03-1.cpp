942c8c72be5006a0c23479f1302374048f66be33
qortjddbs <117032925+qortjddbs@users.noreply.github.com>
2026-01-22 19:50:50 +0900

    문제 03-1 완.

---
+++ p03-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+	int xpos;
+	int ypos;
+
+	void MovePos(int x, int y) {
+		xpos += x;
+		ypos += y;
+	}
+
+	void ShowPosition() const {
+		cout << "[" << xpos << " ," << ypos << "]" << endl;
+	}
+
+	void AddPoint(Point p) {
+		xpos += p.xpos;
+		ypos += p.ypos;
+	}
+};
+
+int main(void) {
+	Point pos1 = { 12, 4 };
+	Point pos2 = { 20, 30 };
+
+	pos1.MovePos(-7, 10);
+	pos1.ShowPosition();
+
+	pos1.AddPoint(pos2);
+	pos1.ShowPosition();
+	return 0;
+}
