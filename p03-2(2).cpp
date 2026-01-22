c9dfa932229fbc74773118816022322bc9e33431
qortjddbs <117032925+qortjddbs@users.noreply.github.com>
2026-01-22 23:33:00 +0900

    문제 03-2(2) 완.

---
+++ p03-2(2).cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+class Printer {
+private:
+	char str[100];
+public:
+	void SetString(const char* s);
+	void ShowString();
+};
+
+void Printer::SetString(const char* s) {
+	strcpy(str, s);
+}
+
+void Printer::ShowString() {
+	cout << str << endl;
+}
+
+int main(void) {
+	Printer pnt;
+	pnt.SetString("Hello world!");
+	pnt.ShowString();
+
+	pnt.SetString("I love C++");
+	pnt.ShowString();
+	return 0;
+}
