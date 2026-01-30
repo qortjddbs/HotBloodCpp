a841b93c7705cf09647c64eb733466e4eab7996c
qortjddbs <117032925+qortjddbs@users.noreply.github.com>
2026-01-31 02:47:29 +0900

    동적할당해서 04-3(2) 완료

---
+++ p04-3(2).cpp
@@ -0,0 +1,125 @@
+//#include <iostream>
+//#include <cstring>
+//using namespace std;
+//
+//namespace COMP_POS {
+//	enum {
+//		CLERK = 1,
+//		SENIOR = 2,
+//		ASSIST = 3,
+//		MANAGER = 4
+//	};
+//}
+//
+//class NameCard {
+//private:
+//	const char* name;
+//	const char* company;
+//	const char* num;
+//	int rank;
+//public:
+//	NameCard(const char* name, const char* company, const char* num, int rank) : name(name), company(company), num(num), rank(rank) {
+//	}
+//	void ShowNameCardInfo() {
+//		cout << "이름 : " << name << endl;
+//		cout << "회사 : " << company << endl;
+//		cout << "전화번호 : " << num << endl;
+//		cout << "직급 : ";
+//		switch (rank) {
+//		case COMP_POS::CLERK:
+//			cout << "사원";
+//			break;
+//		case COMP_POS::SENIOR:
+//			cout << "주임";
+//			break;
+//		case COMP_POS::ASSIST:
+//			cout << "대리";
+//			break;
+//		case COMP_POS::MANAGER:
+//			cout << "과장";
+//			break;
+//		}
+//		cout << endl << endl;
+//	}
+//};
+//
+//int main(void) {
+//	NameCard manClerk("Lee", "ABCEng", "010-1111-2222", COMP_POS::CLERK);
+//	NameCard manSENIOR("Hong", "OrangeEng", "010-3333-4444", COMP_POS::SENIOR);
+//	NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::ASSIST);
+//	manClerk.ShowNameCardInfo();
+//	manSENIOR.ShowNameCardInfo();
+//	manAssist.ShowNameCardInfo();
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+namespace COMP_POS {
+	enum {
+		CLERK = 1,
+		SENIOR = 2,
+		ASSIST = 3,
+		MANAGER = 4
+	};
+}
+
+class NameCard {
+private:
+	char* name;
+	char* company;
+	char* num;
+	int rank;
+public:
+	NameCard(const char* myName, const char* myCompany, const char* myNum, int myRank) {
+		int lenName = strlen(myName) + 1;
+		name = new char[lenName];
+		strcpy(name, myName);
+		int lenCompany = strlen(myCompany) + 1;
+		company = new char[lenCompany];
+		strcpy(company, myCompany);
+		int lenNum = strlen(myNum) + 1;
+		num = new char[lenNum];
+		strcpy(num, myNum);
+		rank = myRank;
+	}
+	~NameCard() {
+		delete[] name;
+		delete[] company;
+		delete[] num;
+	}
+	void ShowNameCardInfo() {
+		cout << "이름 : " << name << endl;
+		cout << "회사 : " << company << endl;
+		cout << "전화번호 : " << num << endl;
+		cout << "직급 : ";
+		switch (rank) {
+		case COMP_POS::CLERK:
+			cout << "사원";
+			break;
+		case COMP_POS::SENIOR:
+			cout << "주임";
+			break;
+		case COMP_POS::ASSIST:
+			cout << "대리";
+			break;
+		case COMP_POS::MANAGER:
+			cout << "과장";
+			break;
+		}
+		cout << endl << endl;
+	}
+};
+
+int main(void) {
+	NameCard manClerk("Lee", "ABCEng", "010-1111-2222", COMP_POS::CLERK);
+	NameCard manSENIOR("Hong", "OrangeEng", "010-3333-4444", COMP_POS::SENIOR);
+	NameCard manAssist("Kim", "SoGoodComp", "010-5555-6666", COMP_POS::ASSIST);
+	manClerk.ShowNameCardInfo();
+	manSENIOR.ShowNameCardInfo();
+	manAssist.ShowNameCardInfo();
+	return 0;
+}
