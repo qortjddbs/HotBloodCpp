1119a05221bd096d0c788bd7f65dbd8289dc2ab2
qortjddbs(notebook) <beak980513@naver.com>
2026-02-08 15:49:32 +0900

    복사 생성자 호출 시기 예제

---
+++ PassObjCopycon.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class SoSimple {
+private:
+	int num;
+public:
+	SoSimple(int n) : num(n) {
+	}
+	SoSimple(const SoSimple& copy) : num(copy.num) {
+		cout << "Called SoSimple(const SoSimple& copy)" << endl;
+	}
+	void ShowData() {
+		cout << "num: " << num << endl;
+	}
+};
+
+void SimpleFuncObj(SoSimple ob) {
+	ob.ShowData();
+}
+
+int main(void) {
+	SoSimple obj(7);
+	cout << "Before SimpleFuncObj()" << endl;
+	SimpleFuncObj(obj);
+	cout << "After SimpleFuncObj()" << endl;
+	return 0;
+}
