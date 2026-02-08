708a6af1f274b5b7db7dad272214c073a10ddb81
qortjddbs(notebook) <beak980513@naver.com>
2026-02-08 14:51:26 +0900

    얕은 복사로 인한 오류 수정

---
+++ ShallowCopyError.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person {
+private:
+	char* name;
+	int age;
+public:
+	Person(const char* myname, int myage) {
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy(name, myname);
+		age = myage;
+	}
+	Person(const Person& copy) : age(copy.age) {
+		name = new char[strlen(copy.name) + 1];
+		strcpy(name, copy.name);
+	}
+	void ShowPersonInfo() {
+				cout << "Name: " << name << endl;
+				cout << "Age: " << age << endl;
+	}
+	~Person()
+	{
+		delete[] name;
+		cout << "Called destructor!" << endl;
+	}
+};
+
+int main(void) {
+	Person man1("Lee dong woo", 29);
+	Person man2 = man1;
+	man1.ShowPersonInfo();
+	man2.ShowPersonInfo();
+	return 0;
+}
