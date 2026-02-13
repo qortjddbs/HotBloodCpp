be3c1a51add5cacf9919ca9759ff592c0b5f57ee
qortjddbs(notebook) <beak980513@naver.com>
2026-02-13 18:41:35 +0900

    p07-1 이렇게 하는 게 맞나?

---
+++ p07-1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Car {		// 기본 연료 자동차
+private:
+	int gasolineGauge;
+public:
+	Car() : gasolineGauge(100) {
+
+	}
+	int GetGasGauge() {
+		return gasolineGauge;
+	}
+};
+
+class HybridCar : public Car {	// 하이브리드 자동차
+private:
+	int electricGauge;
+public:
+	HybridCar(int elec) : electricGauge(elec) {
+
+	}
+	HybridCar() : electricGauge(70) {}
+	int GetElecGauge() {
+		return electricGauge;
+	}
+};
+
+class HybridWaterCar : public HybridCar {	// 하이브리드 워터카
+private:
+	int waterGauge;
+public:
+	HybridWaterCar() : waterGauge(50) {}
+	void ShowCurrentGauge() {
+		cout << "잔여 가솔린: " << GetGasGauge() << endl;
+		cout << "잔여 전기량: " << GetElecGauge() << endl;
+		cout << "잔여 워터량: " << waterGauge << endl;
+	}
+};
+
+int main(void) {
+	HybridWaterCar wc1;
+	wc1.ShowCurrentGauge();
+
+	return 0;
+}
