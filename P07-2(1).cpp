0b379510c4cc1c249423fc2b3080045634763443
qortjddbs(notebook) <beak980513@naver.com>
2026-02-14 16:50:48 +0900

    p07-2(1) 완.

--- P07-2(1).cpp
+++ P07-2(1).cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 class Rectangle {
-
+private:
+	int width;
+	int height;
+public:
+	Rectangle(int w, int h) : width(w), height(h) {}
+	void ShowAreaInfo() const {
+		cout << "¸éÀû: " << width * height << endl;
+	}
 };
 
 class Square : public Rectangle {
-
+private:
+	int side;
+public:
+	Square(int s) : Rectangle(s, s), side(s) {	}
 };
 
 int main(void) {
