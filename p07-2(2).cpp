0f32cad56f329094ced9d8c87037c60b4954206a
qortjddbs(notebook) <beak980513@naver.com>
2026-02-14 17:04:34 +0900

    p07-2(2) 완

--- p07-2(2).cpp
+++ p07-2(2).cpp
@@ -9,14 +9,38 @@ private:
 	char* isbn;		// 국제표준도서번호
 	int price;
 public:
-
+	Book(const char* tit, const char* isb, int pri) : price(pri) {
+		title = new char[strlen(tit) + 1];
+		strcpy(title, tit);
+		isbn = new char[strlen(isb) + 1];
+		strcpy(isbn, isb);
+	}
+	void ShowBookInfo() {
+		cout << "제목: " << title << endl;
+		cout << "ISBN: " << isbn << endl;
+		cout << "가격: " << price << endl;
+	}
+	~Book() {
+		delete[] title;
+		delete[] isbn;
+	}
 };
 
 class EBook : public Book {
 private:
 	char* DRMKey;		// 보안관련 키
 public:
-
+	EBook(const char* tit, const char* isb, int price, const char* key) : Book(tit, isb, price) {
+		DRMKey = new char[strlen(key) + 1];
+		strcpy(DRMKey, key);
+	}
+	void ShowEBookInfo() {
+		ShowBookInfo();
+		cout << "인증키: " << DRMKey << endl;
+	}
+	~EBook() {
+		delete[] DRMKey;
+	}
 };
 
 int main(void) {
