55e96569b324284b5ae95904caf1e9d58dd73f65
qortjddbs <117032925+qortjddbs@users.noreply.github.com>
2026-03-12 01:20:03 +0900

    메인함수 정의 후 문제 시작

---
+++ p11-1(1).cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Gun {
+private:
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum) {}
+	void Shot() {
+		cout << "BBang!" << endl;
+		bullet--;
+	}
+};
+
+class Police {
+private:
+	int handcuffs;
+	Gun* pistol;
+public:
+	Police(int bnum, int bcuff) : handcuffs(bcuff) {
+		if (bnum > 0)
+			pistol = new Gun(bnum);
+		else
+			pistol = NULL;
+	}
+	void PutHandcuff() {
+		cout << "SNAP!" << endl;
+		handcuffs--;
+	}
+	void Shot() {
+		if (pistol == NULL)
+			cout << "Hut BBANG!" << endl;
+		else
+			pistol->Shot();
+	}
+	~Police()
+	{
+		if (pistol != NULL)
+			delete pistol;
+	}
+};
+
+int main(void) {
+	Police pman1(5, 3);
+	Police pman2 = pman1;
+	pman2.PutHandcuff();
+	pman2.Shot();
+
+	Police pman3(2, 4);
+	pman3 = pman1;
+	pman3.PutHandcuff();
+	pman3.Shot();
+}
